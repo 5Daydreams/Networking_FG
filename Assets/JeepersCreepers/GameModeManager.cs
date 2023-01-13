25248f3a595d69fc3281542b7780d0ca1b108664
jeepeer <jespervski@gmail.com>
2023-01-13 15:41:31 +0100

    pray
    more work ?

---
+++ Assets/JeepersCreepers/GameModeManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameModeManager : MonoBehaviour
+{
+    public GameObject menu;
+    private Dropdown dropdown;
+    private Dropdown.OptionData item;
+    private InputField field;
+    private void Start()
+    {
+
+    }
+
+    void SetTimeGameMode()
+    {
+        // enable amount of kills
+
+    }
+    void SetKillGameMode()
+    {
+        // enable amount of time
+
+    }
+}
