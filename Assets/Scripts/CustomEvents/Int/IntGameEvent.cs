e6f8b9682634901906feb64e0fe7f0b45dfa79d4
Nitintito <jason.sandelin@hotmail.com>
2023-01-16 13:19:54 +0100

    Merge branch 'main' into Jason

---
+++ Assets/Scripts/CustomEvents/Int/IntGameEvent.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using submodules.unity_spellbook._Code.CustomEvents.BaseEvent;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "CustomScriptables/Events/IntEvent", fileName = "IntEvent")]
+public class IntGameEvent : BaseGameEvent<int>
+{
+}
