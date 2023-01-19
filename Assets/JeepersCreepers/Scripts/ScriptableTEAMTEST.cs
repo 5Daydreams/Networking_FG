cb969e834590e3a650c1a67baa80c2dc10eb8f96
jeepeer <jespervski@gmail.com>
2023-01-19 14:33:08 +0100

    Revert "Clean up"
    This reverts commit eb0f14ec0e61bb3fef2ae96fc486000609bf3ea9.

---
+++ Assets/JeepersCreepers/Scripts/ScriptableTEAMTEST.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/TEAMTEST", order = 1)]
+public class ScriptableTEAMTEST : ScriptableObject
+{
+    public int[] teamArray = new int[5];
+}
