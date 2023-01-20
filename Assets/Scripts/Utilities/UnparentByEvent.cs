580fc7b553aa90e8f902a3930d35b175720aa7b3
5daydreams <nelsonkossuga@gmail.com>
2023-01-20 14:09:45 +0100

    adjusted the projectile prefab to work with netcode;

--- Assets/Scripts/Utilities/UnparentByEvent.cs
+++ Assets/Scripts/Utilities/UnparentByEvent.cs
@@ -1,19 +1,11 @@
 using Alteruna;
 using UnityEngine;
 
-[RequireComponent(typeof(TransformSynchronizable))]
 public class UnparentByEvent : MonoBehaviour
 {
-    private TransformSynchronizable _transform;
-
-    private void Awake()
-    {
-        _transform = this.GetComponent<TransformSynchronizable>();
-    }
-
     [ContextMenu("Unparent This")]
     public void RemoveParent()
     {
-        this._transform.transform.parent = null;
+        this.transform.parent = null;
     }
 }
