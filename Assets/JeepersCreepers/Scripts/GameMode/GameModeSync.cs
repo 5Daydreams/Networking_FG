f412762ac4cb76ebbb3a86b44ec36eafb6eb9f38
jeepeer <jespervski@gmail.com>
2023-01-25 19:55:04 +0100

    Team deathmatch win condition synced

--- Assets/JeepersCreepers/Scripts/GameMode/GameModeSync.cs
+++ Assets/JeepersCreepers/Scripts/GameMode/GameModeSync.cs
@@ -7,12 +7,15 @@ public class GameModeSync : AttributesSync
 {
     [SynchronizableField] [SerializeField] private int redTeamScore;
     [SynchronizableField] [SerializeField] private int blueTeamScore;
-
+    [SerializeField] private GameObject redWin;
+    [SerializeField] private GameObject blueWin;
     [SerializeField] private GameModeUI UI;
 
     public int GetRedTeamScore() { return redTeamScore; }
     public int GetBlueTeamScore() { return blueTeamScore; }
 
+    private bool bRedUIIsActive = false;
+    private bool bBlueUIIsActive = false;
     public void HandleRedTeamScore()
     {
         redTeamScore++;
@@ -48,6 +51,34 @@ public class GameModeSync : AttributesSync
     public void HandleUIReset()
     {
         UI.UpdateRedScore(0);
-        UI.UpdateRedScore(0);
+        UI.UpdateBlueScore(0);
+    }
+
+    public void HandleWinScreen(int team)
+    {
+        switch (team)
+        {
+            case (int)Team.red:
+                BroadcastRemoteMethod("ToggleRedWinUI");
+                break;
+
+            case (int)Team.blue:
+                BroadcastRemoteMethod("ToggleBlueWinUI");
+                break;
+        }
+    }
+
+    [SynchronizableMethod]
+    public void ToggleRedWinUI()
+    {
+        bRedUIIsActive = !bRedUIIsActive;
+        redWin.SetActive(bRedUIIsActive);
+    }
+
+    [SynchronizableMethod]
+    public void ToggleBlueWinUI()
+    {
+        bBlueUIIsActive = !bBlueUIIsActive;
+        blueWin.SetActive(bBlueUIIsActive);
     }
 }
